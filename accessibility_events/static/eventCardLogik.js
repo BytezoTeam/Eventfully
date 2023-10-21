48ff51429223498758803af475f756f97c8ba255
BlitzBenedikt <117511666+BlitzBenedikt@users.noreply.github.com>
2023-10-21 15:34:04 +0200

    mehr logik

---
+++ accessibility_events/static/eventCardLogik.js
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"></link>
+<html>
+<head>
+	<title>Event</title>
+</head>
+<body>
+	<article>
+		<h2>Event Titel</h2>
+
+		<p>Event Beschreibung</p>
+
+		<footer>
+        <i class="fas fa-euro-sign"></i>
+        Event Preis <br />
+        <i class="fas fa-map-pin"></i>
+        Event Ort <br />
+        <i class="far fa-calendar-alt"></i>
+        Event Startdatum <br />
+        <i class="fas fa-tags"></i> Event Themen <br>
+
+    <i class="fas fa-globe"></i> <a href="Event Link">Event Link</a>
+</footer>
+
+	</article>
+
+    <script src="/path/to/your/script.js"></script> <!--Pfad zu JavaScript-->
+    
+ </body>
+
+ </html> 
