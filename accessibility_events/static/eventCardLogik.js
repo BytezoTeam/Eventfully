dd8d5cf1c92f6d7f19316dbe22d21f0117ddda4f
BlitzBenedikt <117511666+BlitzBenedikt@users.noreply.github.com>
2023-10-21 15:43:40 +0200

    feler behoben

--- accessibility_events/static/eventCardLogik.js
+++ accessibility_events/static/eventCardLogik.js
@@ -1,31 +1,33 @@
-<!DOCTYPE html>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"></link>
-<html>
-<head>
-	<title>Event</title>
-</head>
-<body>
-	<article>
-		<h2>Event Titel</h2>
+// This is a comment, which is a valid JavaScript expression.
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faEuroSign,
+  faMapPin,
+  faCalendarAlt,
+  faTags,
+  faGlobe,
+} from "@fortawesome/free-solid-svg-icons";
 
-		<p>Event Beschreibung</p>
+function EventCard() {
+  return (
+    <div>
+      <h2>Event Titel</h2>
+      <p>Event Beschreibung</p>
+      <footer>
+        <div>
+          <FontAwesomeIcon icon={faEuroSign} />
+          Event Preis <br />
+          <FontAwesomeIcon icon={faMapPin} />
+          Event Ort <br />
+          <FontAwesomeIcon icon={faCalendarAlt} />
+          Event Startdatum <br />
+          <FontAwesomeIcon icon={faTags} /> Event Themen <br />
+          <FontAwesomeIcon icon={faGlobe} /> <a href="Event Link">Event Link</a>
+        </div>
+      </footer>
+    </div>
+  );
+}
 
-		<footer>
-        <i class="fas fa-euro-sign"></i>
-        Event Preis <br />
-        <i class="fas fa-map-pin"></i>
-        Event Ort <br />
-        <i class="far fa-calendar-alt"></i>
-        Event Startdatum <br />
-        <i class="fas fa-tags"></i> Event Themen <br>
-
-    <i class="fas fa-globe"></i> <a href="Event Link">Event Link</a>
-</footer>
-
-	</article>
-
-    <script src="/path/to/your/script.js"></script> <!--Pfad zu JavaScript-->
-    
- </body>
-
- </html> 
+export default EventCard;
