6c13cce39aee9d023221cfdeb4580ec42343bc6e
2mal3 <56305732+2mal3@users.noreply.github.com>
2024-01-06 00:13:21 +0100

    ref: emails code cleanup

--- eventfully/emails.py
+++ eventfully/emails.py
@@ -5,13 +5,17 @@ from imap_tools import MailBox
 
 clear_Emails = False
 
+load_dotenv()
+EMAIL = getenv("EMAIL")
+PASS = getenv("PASS")
+SERVER = getenv("SERVER")
 
-def get_emails(EMAIL, PASS, SERVER):
-    global emails
+
+def get_emails(email: str, password: str, server: str) -> dict[str, dict[str, str]]:
     emails = {}
     # Login into the server 
     load_dotenv()
-    with MailBox(SERVER).login(EMAIL, PASS, 'INBOX') as mailbox:
+    with MailBox(server).login(email, password, 'INBOX') as mailbox:
         # Fetch the emails from inbox
         for msg in mailbox.fetch():
             body = msg.text or msg.html
@@ -23,12 +27,13 @@ def get_emails(EMAIL, PASS, SERVER):
             }
             print(f"Got EMail with subject '{subject}'")
 
+    return emails
 
-def writeEmails(emails):
+
+def write_emails(emails: dict[str, dict[str, str]]):
     # Write Data into Database
     # Check if the email already exists in the database
     for subject, body in emails.items():
-        msg = body["msg"]
         body = body["body"]
         if not EMailContent.select().where(EMailContent.subject == subject).exists():
             # Add the email to the database
@@ -38,23 +43,20 @@ def writeEmails(emails):
             print(f"EMail with subject '{subject}' already exists in database")
 
 
-def clearEmails():
+def clear_emails():
     # Clear all emails from the database
     EMailContent.delete().execute()
     print("Cleared all emails from database")
 
 
 def main():
-    # Get the environment variables
-    load_dotenv()
-
-    EMAIL = getenv("EMAIL")
-    PASS = getenv("PASS")
-    SERVER = getenv("SERVER")
-
-    get_emails(EMAIL, PASS, SERVER)
+    emails = get_emails(EMAIL, PASS, SERVER)
     # Write emails into database
-    writeEmails(emails)
+    write_emails(emails)
     # Clear emails from database (if wanted)
     if clear_Emails:
-        clearEmails()
+        clear_emails()
+
+
+if __name__ == "__main__":
+    main()
