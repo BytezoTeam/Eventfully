e111c3276d93167f091fe51523a439e6178f616e
2mal3 <56305732+2mal3@users.noreply.github.com>
2024-01-27 12:34:29 +0100

    ref(scraping): more functional approach
    no global variables anymore

--- eventfully/scraping.py
+++ eventfully/scraping.py
@@ -8,18 +8,18 @@ from selenium.webdriver.support import expected_conditions as EC
 import eventfully.utils as utils
 import eventfully.database as db
 
-chrome_options = Options()
-chrome_options.add_argument("--headless")
-browser = webdriver.Chrome(chrome_options)
-
 
 def main():
-    unbezahlbar()
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    browser = webdriver.Chrome(chrome_options)
+
+    unbezahlbar(browser)
 
     browser.quit()
 
 
-def unbezahlbar():
+def unbezahlbar(browser: webdriver.Chrome):
     browser.get("https://www.zuerichunbezahlbar.ch/events/")
 
     for _ in range(6):
@@ -32,13 +32,13 @@ def unbezahlbar():
                 continue
 
             try:
-                title = get_element(By.CSS_SELECTOR,
+                title = get_element(browser, By.CSS_SELECTOR,
                                     ".reveal-modal.open .poster__title-span.poster__title-span-text").text
-                time = get_element(By.CSS_SELECTOR, ".detailpost__date time").text
-                info = get_element(By.CSS_SELECTOR, "div.detailpost__info").text
-                address = get_element(By.CSS_SELECTOR, "address.detailpost__address").text
-                description = get_element(By.CSS_SELECTOR, "div.detailpost__description").text
-                link = get_element(By.CSS_SELECTOR, "a.detailpost__link").get_attribute("href")
+                time = get_element(browser, By.CSS_SELECTOR, ".detailpost__date time").text
+                info = get_element(browser, By.CSS_SELECTOR, "div.detailpost__info").text
+                address = get_element(browser, By.CSS_SELECTOR, "address.detailpost__address").text
+                description = get_element(browser, By.CSS_SELECTOR, "div.detailpost__description").text
+                link = get_element(browser, By.CSS_SELECTOR, "a.detailpost__link").get_attribute("href")
             except (
                     selenium.common.exceptions.TimeoutException,
                     selenium.common.exceptions.ElementClickInterceptedException):
@@ -56,15 +56,15 @@ def unbezahlbar():
                                    content=f"title: {title}\ntime_date: {time}\n info: {info}\naddress: {address}\ndescription: {description}\nlink: {link}\naddress: {address}\ncity: Zürich")
 
         # get_element(By.CSS_SELECTOR, "span.step-links a").click()
-        get_element(By.XPATH, "//a[text()='weiter »']").click()
+        get_element(browser, By.XPATH, "//a[text()='weiter »']").click()
 
 
-def get_element(selector_type, selector: str):
+def get_element(browser: webdriver.Chrome, selector_type: str, selector: str):
     return WebDriverWait(browser, 5).until(
         EC.presence_of_element_located((selector_type, selector)))
 
 
-def get_elements(selector_type, selector: str):
+def get_elements(browser: webdriver.Chrome, selector_type: str, selector: str):
     return WebDriverWait(browser, 5).until(
         EC.presence_of_all_elements_located((selector_type, selector)))
 
