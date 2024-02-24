7dabbad09f44185ccc3d02712e560b5d90d5eb23
2mal3 <56305732+2mal3@users.noreply.github.com>
2024-02-24 23:58:15 +0100

    ref: better basic ai provider

--- eventfully/sources/ai_provider.py
+++ eventfully/sources/ai_provider.py
@@ -3,28 +3,28 @@ from os import getenv
 
 from beartype import beartype
 from dotenv import load_dotenv
-from openai import OpenAI
+from openai import OpenAI, ChatCompletion
+from result import Result, Ok, Err
+from tenacity import retry, wait_random_exponential, stop_after_attempt, retry_if_result
 
 load_dotenv()
 OPENAI_API_KEY = getenv("OPENAI_API_KEY")
-MODEL = "gpt-3.5-turbo-0125"    # "gpt-4-turbo-preview" for better results but ~20x the cost
+GPT_MODEL = "gpt-3.5-turbo-0125"    # "gpt-4-turbo-preview" for better results but ~20x the cost
 
 _client = OpenAI(api_key=OPENAI_API_KEY)
 
 
-@lru_cache(maxsize=64)
+@lru_cache(maxsize=16)
+@retry(wait=wait_random_exponential(max=5), stop=stop_after_attempt(3), retry=retry_if_result(lambda r: r.is_err()))
 @beartype
-def generate_completion(system: str, user: str) -> str:
-    system_prompt = {
-        "role": "system",
-        "content": system
-    }
-    user_prompt = {
-        "role": "user",
-        "content": user
-    }
-    chat_completion = _client.chat.completions.create(
-        messages=[system_prompt, user_prompt],
-        model=MODEL
-    )
-    return chat_completion.choices[0].message.content
+def chat_completion_request(messages, tools=None, tool_choice=None, model=GPT_MODEL) -> Result[ChatCompletion, Exception]:
+    try:
+        response = _client.chat.completions.create(
+            model=model,
+            messages=messages,
+            tools=tools,
+            tool_choice=tool_choice,
+        )
+        return Ok(response)
+    except Exception as e:
+        return Err(e)
