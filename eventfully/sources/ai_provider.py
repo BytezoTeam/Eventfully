bce8c946df386a386fc3ea5ba5a8d84928411985
2mal3 <56305732+2mal3@users.noreply.github.com>
2024-03-04 21:10:07 +0100

    ref: improved ai provider

--- eventfully/sources/ai_provider.py
+++ eventfully/sources/ai_provider.py
@@ -1,9 +1,8 @@
-from functools import lru_cache
 from os import getenv
 
-from beartype import beartype
 from dotenv import load_dotenv
-from openai import OpenAI, ChatCompletion
+from openai import OpenAI
+from openai.types.chat.chat_completion import ChatCompletion
 from result import Result, Ok, Err
 from tenacity import retry, wait_random_exponential, stop_after_attempt, retry_if_result
 
@@ -14,10 +13,8 @@ GPT_MODEL = "gpt-3.5-turbo-0125"    # "gpt-4-turbo-preview" for better results b
 _client = OpenAI(api_key=OPENAI_API_KEY)
 
 
-@lru_cache(maxsize=16)
 @retry(wait=wait_random_exponential(max=5), stop=stop_after_attempt(3), retry=retry_if_result(lambda r: r.is_err()))
-@beartype
-def chat_completion_request(messages, tools=None, tool_choice=None, model=GPT_MODEL) -> Result[ChatCompletion, Exception]:
+def chat_completion_request(messages: list[dict[str, any]], tools=None, tool_choice=None, model=GPT_MODEL) -> Result[ChatCompletion, Exception]:
     try:
         response = _client.chat.completions.create(
             model=model,
