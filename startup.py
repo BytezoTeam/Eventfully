33db5a7c3814bbc2ac149461643ff2991a201029
LuisSchuimer <luis.schuimer@outlook.com>
2024-03-05 22:18:49 +0100

    Add EMailContent table and add startup script (Right now only for windows)

---
+++ startup.py
@@ -0,0 +1,32 @@
+import os
+import subprocess
+import requests
+
+meilisearch_running = False
+MEILI_DIR = ""
+MEILI_FILE = ""
+MEILI_KEY = ""
+
+print("Running Startup")
+
+try:
+    requests.get("http://localhost:7700")
+    print("Meilisearch is already running")
+    meilisearch_running = True
+
+except requests.exceptions.ConnectionError:
+    print("Meilisearch is not running")
+    print("Starting Meilisearch")
+    command = f'start cmd /k "{os.path.join(MEILI_DIR, MEILI_FILE)} --master-key={MEILI_KEY}"'
+    subprocess.Popen(command, shell=True, cwd=MEILI_DIR)
+
+try:
+    print("Update database.db? y/n? (Will delete all data in database)")
+    if input() == "y":
+        os.remove("database.db")
+        print("Deleted database.db")
+        import eventfully.database
+        print("Updated database.db")
+except PermissionError:
+    print("database.db is in use. Please close the file and run this script again.")
+    exit(1)
