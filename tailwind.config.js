536ff7faecd6c12ec8418c2fdc700e9b6f1688a1
2mal3 <56305732+2mal3@users.noreply.github.com>
2024-04-02 22:37:56 +0200

    chore: proper tailwindcss build system

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./eventfully/templates/**/*.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require("daisyui")
+  ],
+}
