b8f9641f1001e692a3c16b4e514321212eb96d1d
2mal3 <56305732+2mal3@users.noreply.github.com>
2024-05-02 17:15:42 +0200

    chore: removed file logger for now

--- eventfully/logger.py
+++ eventfully/logger.py
@@ -1,7 +1,4 @@
 import logging
-from os import path
-
-from get_project_root import root_path
 
 log = logging.getLogger("eventfully")
 log.setLevel(logging.DEBUG)
@@ -15,10 +12,3 @@ _console_handler = logging.StreamHandler()
 _console_handler.setLevel(_LEVEL)
 _console_handler.setFormatter(_formatter)
 log.addHandler(_console_handler)
-
-
-_LOG_FILE_PATH = path.join(root_path(ignore_cwd=True), "latest.log")
-_file_handler = logging.FileHandler(_LOG_FILE_PATH)
-_file_handler.setLevel(_LEVEL)
-_file_handler.setFormatter(_formatter)
-log.addHandler(_file_handler)
