21f4ffd9d84ca3d5e2cc68d7ced59f42bd591c76
2mal3 <56305732+2mal3@users.noreply.github.com>
2024-05-08 12:20:15 +0200

    tests: integration of source search & post process

--- tests/search/test_sources.py
+++ tests/search/test_sources.py
@@ -1,42 +1,20 @@
 from datetime import datetime
-from typing import Callable
 
 import pytest
 
-from eventfully.database import schemas
-from eventfully.search.sources import kulturloewen, zuerichunbezahlbar
+from eventfully.search.sources import kulturloewen, zuerichunbezahlbar, neanderticket
 
 
-@pytest.mark.parametrize("search_source", [kulturloewen.search, zuerichunbezahlbar.search])
-def test_search(search_source: Callable):
-    search_result = search_source("", datetime.today(), datetime.today())
-    print(search_result)
-    assert len(search_result) > 0
+@pytest.mark.parametrize("source,required_fields", [
+    (zuerichunbezahlbar, ["description", "address", "city", "operator_web_link"]),
+    (kulturloewen, ["description", "address", "city"]),
+    (neanderticket, ["description", "address"]),
+])
+def test_source_integration(source, required_fields: list[str]):
+    raw_events = source.search("", datetime.today(), datetime.today(), "")
+    assert len(raw_events) > 0
 
-
-@pytest.mark.parametrize(
-    "post_process_function,web_link,required_fields",
-    [
-        (
-            zuerichunbezahlbar.post_process,
-            "https://www.zuerichunbezahlbar.ch/events/kultur-nachtleben/film/2024-04-25-cinelowenbraukunst-screening/",
-            ["description", "address", "operator_web_link"],
-        ),
-        (
-            kulturloewen.post_process,
-            "https://www.neanderticket.de/486897",
-            ["description", "address"],
-        ),
-    ],
-)
-def test_post_processing(
-    post_process_function: Callable, web_link: str, required_fields: list[str]
-):
-    raw_event = schemas.Event(
-        web_link=web_link, start_time=datetime.now(), end_time=datetime.now(), source=""
-    )
-    event = post_process_function(raw_event)
-    print(event)
+    event = source.post_process(list(raw_events)[0])
 
     for field in required_fields:
         field_content = getattr(event, field)
