90faf494dd969366a0f468bfa66b72cc1dca8b0d
2mal3 <56305732+2mal3@users.noreply.github.com>
2024-05-21 19:24:57 +0200

    tests: also test crawl sources

--- tests/search/test_sources.py
+++ tests/search/test_sources.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 import pytest
 
-from eventfully.search.sources import kulturloewen, zuerichunbezahlbar, neanderticket
+from eventfully.search.sources import kulturloewen, zuerichunbezahlbar, neanderticket, bpb, boundicca
 
 
 @pytest.mark.parametrize(
@@ -13,7 +13,7 @@ from eventfully.search.sources import kulturloewen, zuerichunbezahlbar, neandert
         (neanderticket, ["description", "address", "image_link"]),
     ],
 )
-def test_source_integration(source, required_fields: list[str]):
+def test_search_integration(source, required_fields: list[str]):
     raw_events = source.search("", datetime.today(), datetime.today(), "")
     assert len(raw_events) > 0
 
@@ -24,3 +24,22 @@ def test_source_integration(source, required_fields: list[str]):
         assert field_content
         assert len(field_content) > 0
         assert field_content.strip() == field_content
+
+
+@pytest.mark.parametrize(
+    "source,required_fields",
+    [
+        (bpb, ["source", "title", "image_link", "description", "category"]),
+        (boundicca, ["source", "title"]),
+    ]
+)
+def test_crawl(source, required_fields: list[str]):
+    events = source.crawl()
+
+    for event in events:
+        for field in required_fields:
+            print(field)
+            field_content = getattr(event, field)
+            assert field_content
+            assert len(field_content) > 0
+            assert field_content.strip() == field_content
