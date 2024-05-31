f2cedf2eb6f9c3ab5e9b4b2a81dc35d3424b6da6
2mal3 <56305732+2mal3@users.noreply.github.com>
2024-05-31 16:36:31 +0200

    ref(neanderticket): split to large method

--- eventfully/search/sources/neanderticket.py
+++ eventfully/search/sources/neanderticket.py
@@ -3,7 +3,7 @@ from datetime import datetime, timedelta
 
 from beartype import beartype
 import niquests
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, PageElement
 
 from eventfully.database import schemas
 
@@ -29,59 +29,65 @@ def search(therm: str, min_time: datetime, max_time: datetime, city: str) -> set
         soup = BeautifulSoup(request.text, "html.parser")
         raw_events = soup.find_all("div", class_="kurz-rahmen")
         for raw_event in raw_events:
-            image_object = raw_event.find("a", class_="fancybox")
-            if image_object:
-                image_path = image_object.get("href")
-                image_link = f"https://www.neanderticket.de{image_path}"
-            else:
-                image_link = None
-
-            # Extract the start time from the html
-            start_time_object = raw_event.find("div", class_="beginn")
-            if not start_time_object:  # Skip if there is no start time because its required
-                continue
-            raw_start_time = start_time_object.text.strip().split(":")
-            start_time = datetime(
-                year=search_time.year,
-                month=search_time.month,
-                day=search_time.day,
-                hour=int(raw_start_time[0]),
-                minute=int(raw_start_time[1]),
-            )
-
-            # Extract the end time from the html **if** there is one
-            end_time_object = raw_event.find("div", class_="ende")
-            if end_time_object and "Ende" in end_time_object.text:  # indicator that the event has an end time
-                raw_end_time = end_time_object.text.strip().split(": ")[1].split(":")
-                h = int(raw_end_time[0])
-                m = int(raw_end_time[1])
-                end_time = datetime(
-                    year=search_time.year, month=search_time.month, day=search_time.day, hour=h, minute=m
-                )
-            else:
-                end_time = start_time
-
-            title = raw_event.find("h1").text.strip()
-
-            event_id = raw_event.find("a", class_="aufklapplink").get("id").removeprefix("e")
-            web_link = f"https://www.neanderticket.de/?{event_id}"
-
-            events.add(
-                schemas.Event(
-                    web_link=web_link,
-                    start_time=start_time,
-                    end_time=end_time,
-                    source="neanderticket",
-                    title=title,
-                    image_link=image_link,
-                    city=city if city != "" else None,
-                    category="culture",
-                )
-            )
+            if event := _extract_event_from_html(raw_event, search_time, city):
+                events.add(event)
 
         search_time += timedelta(days=1)
 
     return events
+
+
+@beartype
+def _extract_event_from_html(raw_event: PageElement, search_time: datetime, city: str) -> schemas.Event | None:
+    image_object = raw_event.find_next("a", class_="fancybox")
+    if image_object:
+        image_path = image_object.get("href")
+        image_link = f"https://www.neanderticket.de{image_path}"
+    else:
+        image_link = None
+
+    # Extract the start time from the html
+    start_time_object = raw_event.find_next("div", class_="beginn")
+    if not start_time_object:  # Skip if there is no start time because its required
+        return None
+    raw_start_time = start_time_object.text.strip().split(":")
+    start_time = datetime(
+        year=search_time.year,
+        month=search_time.month,
+        day=search_time.day,
+        hour=int(raw_start_time[0]),
+        minute=int(raw_start_time[1]),
+    )
+
+    # Extract the end time from the html **if** there is one
+    end_time_object = raw_event.find_next("div", class_="ende")
+    if end_time_object and "Ende" in end_time_object.text:  # indicator that the event has an end time
+        raw_end_time = end_time_object.text.strip().split(": ")[1].split(":")
+        h = int(raw_end_time[0])
+        m = int(raw_end_time[1])
+        end_time = datetime(
+            year=search_time.year, month=search_time.month, day=search_time.day, hour=h, minute=m
+        )
+    else:
+        end_time = start_time
+
+    title = raw_event.find_next("h1").text.strip()
+
+    event_id = raw_event.find_next("a", class_="aufklapplink").get("id").removeprefix("e")
+    web_link = f"https://www.neanderticket.de/?{event_id}"
+
+    event = schemas.Event(
+        web_link=web_link,
+        start_time=start_time,
+        end_time=end_time,
+        source="neanderticket",
+        title=title,
+        image_link=image_link,
+        city=city if city != "" else None,
+        category="culture",
+    )
+
+    return event
 
 
 @beartype
