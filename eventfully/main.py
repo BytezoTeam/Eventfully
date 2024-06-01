2bc4b20dbc3d8705773d238211253a18f30e4431
2mal3 <56305732+2mal3@users.noreply.github.com>
2024-06-01 13:11:12 +0200

    ref: unified jwt handling
    fix
    
    ref: de-duplicate code

--- eventfully/main.py
+++ eventfully/main.py
@@ -1,5 +1,6 @@
 import atexit
 from datetime import datetime, timedelta
+from functools import wraps
 from http import HTTPStatus
 from os import getenv
 from random import randint
@@ -7,7 +8,7 @@ from uuid import uuid4
 
 import jwt
 from dotenv import load_dotenv
-from flask import Flask, render_template, request, make_response
+from flask import Flask, render_template, request, make_response, Response
 from flask_apscheduler import APScheduler
 from flask_wtf import FlaskForm
 from sqids import Sqids
@@ -41,6 +42,55 @@ scheduler.add_job("collect", crawl.main, trigger="cron", day="*", max_instances=
 scheduler.start()
 
 
+# The main http routes / flask logic
+def jwt_check(deny_unauthenticated=False):
+    """
+    Checks if a valid jwt token is present in the request cookies.
+    Aborts the request if the token is not valid or expired and deny_unauthenticated is True.
+    If the token is valid, the function is called with the user_id as a string for the first argument.
+    """
+
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            jwt_token = request.cookies.get("jwt_token")
+            if not jwt_token:
+                if deny_unauthenticated:
+                    return "", HTTPStatus.UNAUTHORIZED
+                else:
+                    return func(None, *args, **kwargs)
+
+            # FIXME: Probably needs a try/except
+            content = jwt.decode(jwt_token, JWT_KEY, algorithms=["HS256"])
+
+            # Token is too old and should be deleted
+            expire_date = datetime.fromtimestamp(content["expire_date"])
+            if expire_date <= datetime.now() - timedelta(days=ID_EXPIRE_TIME):
+                if deny_unauthenticated:
+                    response = make_response()
+                    response.delete_cookie("jwt_token")
+                    return response, HTTPStatus.UNAUTHORIZED
+                else:
+                    return func(None, *args, **kwargs)
+
+            return func(content["user_id"], *args, **kwargs)
+
+        return wrapper
+
+    return decorator
+
+
+def add_jwt_cookie_to_response(response: Response, user_id: str) -> Response:
+    expire_date = datetime.now() + timedelta(days=ID_EXPIRE_TIME)
+    response.set_cookie(
+        "jwt_token",
+        jwt.encode({"user_id": user_id, "expire_date": datetime.timestamp(expire_date)}, JWT_KEY, algorithm="HS256"),
+        expires=expire_date,
+        httponly=True,
+    )
+    return response
+
+
 @app.errorhandler(500)
 def internal_error_server_error(error):
     log.error("Internal Server Error: " + str(error))
@@ -49,51 +99,23 @@ def internal_error_server_error(error):
 
 # Routes
 @app.route("/", methods=["GET"])
-def index():
-    user_id = request.cookies.get("user_id")
-    jwt_token = request.cookies.get("jwt_token")
-
-    if jwt_token:
-        try:
-            encoded_jwt = jwt.decode(jwt_token, JWT_KEY, algorithms=["HS256"])
-        except Exception:
-            return "", HTTPStatus.UNAUTHORIZED
-
-        if datetime.fromtimestamp(encoded_jwt["expire_date"]) <= datetime.now() - timedelta(days=ID_EXPIRE_TIME):
-            log.debug("Deleting JWT")
-            response = make_response(render_template("index.html"))
-            response.delete_cookie("jwt_token")
-
-            return response
-
-        else:
-            if encoded_jwt["user_id"] == user_id:
-                is_signed_in = crud.check_user_exists(user_id)
-                if is_signed_in:
-                    user = crud.get_user_data(user_id)
-                    username = user["name"]
-                else:
-                    username = None
-
-                return render_template("index.html", logged_in=is_signed_in, username=username)
-
-            else:
-                return "", HTTPStatus.UNAUTHORIZED
-    else:
-        username = None
-
+@jwt_check()
+def index(user_id: str):
     cities = crud.get_possible_cities()
 
-    return render_template("index.html", logged_in=is_signed_in, username=username, cities=cities)
+    if not user_id:
+        return render_template("index.html", logged_in=False, cities=crud.get_possible_cities())
+
+    user = crud.get_user_data(user_id)
+    username = user["name"]
+
+    return render_template("index.html", logged_in=True, username=username, cities=cities)
 
 
 @app.route("/api/toggle_event_like")
-def toggle_event_like():
+@jwt_check()
+def toggle_event_like(user_id: str):
     event_id = request.args.get("id")
-    user_id = request.cookies.get("user_id")
-
-    if not user_id:
-        return "", 401
 
     log.debug(f"Event '{event_id}' like toggled for '{user_id}'")
     liked_events = crud.get_liked_event_ids_by_user_id(user_id)
@@ -106,37 +128,31 @@ def toggle_event_like():
 
 
 @app.route("/api/group/share")
-def share_event():
+@jwt_check(deny_unauthenticated=True)
+def share_event(user_id: str):
     event_id = request.args.get("event_id")
     group_id = request.args.get("group_id")
-    user_id = request.cookies.get("user_id")
 
-    if not user_id:
-        return "", 401
-
-    log.debug(f"Event {event_id} shared to {group_id} by {user_id}")
     crud.like_event(user_id, event_id, group_id)
+    log.debug(f"Event {event_id} shared to {group_id} by {user_id}")
 
     return "", HTTPStatus.OK
 
 
 @app.route("/api/group/create")
-def create_group():
-    user_id = request.cookies.get("user_id")
+@jwt_check(deny_unauthenticated=True)
+def create_group(user_id: str):
     group_name = request.args.get("group_name")
 
-    if user_id:
-        cool_id = Sqids().encode([randint(0, int(1e15))])
-        crud.add_group(user_id, cool_id, group_name)
+    cool_id = Sqids().encode([randint(0, int(1e15))])
+    crud.add_group(user_id, cool_id, group_name)
 
-        return "", HTTPStatus.OK
-
-    return "", HTTPStatus.BAD_REQUEST
+    return "", HTTPStatus.OK
 
 
 @app.route("/api/group/share")
-def share_to_group():
-    user_id = request.cookies.get("user_id")
+@jwt_check(deny_unauthenticated=True)
+def share_to_group(user_id: str):
     event_id = request.args.get("id")
     group_id = request.args.get("group_id")
 
@@ -146,24 +162,23 @@ def share_to_group():
 
 
 @app.route("/api/group/add_member")
-def add_member():
-    user_id = request.args.get("user_id")
+@jwt_check(deny_unauthenticated=True)
+def add_member(user_id: str):
     group_id = request.args.get("group_id")
 
-    if crud.member_is_admin(request.cookies.get("user_id"), group_id=group_id):
+    if crud.member_is_admin(user_id, group_id=group_id):
         crud.add_member_to_group(user_id, group_id, False)
 
     return "", 200
 
 
 @app.route("/api/account/delete", methods=["POST"])
-def delete_account():
-    user_id = request.cookies.get("user_id")
-
+@jwt_check(deny_unauthenticated=True)
+def delete_account(user_id: str):
     crud.delete_account(user_id)
 
     response = make_response()
-    response.delete_cookie("user_id")
+    response.delete_cookie("jwt_token")
     return response, HTTPStatus.OK
 
 
@@ -175,8 +190,6 @@ class SignUpForm(FlaskForm):
 
 @app.route("/api/account/signup", methods=["POST"])
 def signup_account():
-    load_dotenv()
-
     form = SignUpForm()
     if not form.validate():
         return form.errors, HTTPStatus.BAD_REQUEST
@@ -184,15 +197,7 @@ def signup_account():
     user_id = str(uuid4())
     crud.create_account(form.username.data, form.password.data, user_id, form.email.data)
 
-    response = make_response()
-    expire_date = datetime.now() + timedelta(days=ID_EXPIRE_TIME)
-    response.set_cookie("user_id", user_id, httponly=True, expires=expire_date)
-    expire_date = datetime.timestamp(datetime.now() + timedelta(days=ID_EXPIRE_TIME))
-    response.set_cookie(
-        "jwt_token",
-        jwt.encode({"user_id": user_id, "expire_date": expire_date}, JWT_KEY, algorithm="HS256"),
-        httponly=True,
-    )
+    response = add_jwt_cookie_to_response(make_response(), user_id)
 
     log.info(f"User '{form.username.data}' signed up with user_id '{user_id}'")
 
@@ -215,40 +220,29 @@ def signin_account():
     if not user_id:
         return make_response(), HTTPStatus.UNAUTHORIZED
 
-    response = make_response()
-    expire_date = datetime.now() + timedelta(days=ID_EXPIRE_TIME)
-    response.set_cookie("user_id", user_id, httponly=True, expires=expire_date)
-    expire_date = datetime.timestamp(datetime.now() + timedelta(days=ID_EXPIRE_TIME))
-    response.set_cookie(
-        "jwt_token",
-        jwt.encode({"user_id": user_id, "expire_date": expire_date}, JWT_KEY, algorithm="HS256"),
-        httponly=True,
-    )
+    response = add_jwt_cookie_to_response(make_response(), user_id)
+
     return response, HTTPStatus.OK
 
 
 @app.route("/api/account/signout", methods=["POST"])
+@jwt_check(deny_unauthenticated=True)
 def signout_account():
     response = make_response()
-    response.delete_cookie("user_id")
     response.delete_cookie("jwt_token")
 
     return response, HTTPStatus.OK
 
 
 @app.route("/api/search", methods=["GET"])
-def get_events():
+@jwt_check()
+def get_events(user_id: str):
     therm = request.args.get("therm", "")
     city = request.args.get("city", "")
 
-    user_id = request.cookies.get("user_id")
-
     liked_events = crud.get_liked_event_ids_by_user_id(user_id) if crud.check_user_exists(user_id) else []
-
     groups = crud.get_groups_of_member(user_id) if crud.check_user_exists(user_id) else {}
-
     share_events = {}
-
     for group in groups:
         share_events[groups[group]] = crud.get_shared_events(group)
 
