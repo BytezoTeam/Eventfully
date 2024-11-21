fc48c7c29712457fb309649574e2f003bbfd1e78
2mal3 <56305732+2mal3@users.noreply.github.com>
2024-11-21 21:59:30 +0100

    ref!: handle whole config in one single file & rename some (#111)

--- eventfully/database/database.py
+++ eventfully/database/database.py
@@ -1,25 +1,19 @@
 import atexit
-from os import getenv, path
+from os import path
 from random import choice
 
 import meilisearch as ms
-from dotenv import load_dotenv
 from get_project_root import root_path
 from peewee import Model
 from playhouse.sqlite_ext import SqliteExtDatabase
 from playhouse.shortcuts import ThreadSafeDatabaseMetadata
 
-load_dotenv()
-_MEILI_HOST = getenv("MEILI_HOST")
-_MEILI_MASTER_KEY = getenv("MEILI_MASTER_KEY")
+from eventfully.config import CONFIG
+
 _SQL_DB_PATH = path.join(root_path(ignore_cwd=True), "database", "sqlite", "database.db")
-if not _MEILI_MASTER_KEY:
-    raise ValueError("No MeiliSearch key provided. Please set MEILI_KEY in .env file.")
-if not _MEILI_HOST:
-    raise ValueError("No MeiliSearch host provided. Please set MEILI_HOST in .env file.")
 
 # Meilisearch
-ms_client = ms.Client(_MEILI_HOST, _MEILI_MASTER_KEY)
+ms_client = ms.Client(CONFIG.MEILI_HOST, CONFIG.MEILI_MASTER_KEY)
 if not ms_client.is_healthy():
     FUNNY_ERRORS = [
         "Oh, trying to connect to Meilisearch, are we? Did you make sure it's not on a coffee break? Check if it's running and the .env file isn't just a decorative piece.",
