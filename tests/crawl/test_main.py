48dc5558e4c38c6fea228172afe6852152a15440
2mal3 <56305732+2mal3@users.noreply.github.com>
2025-04-10 15:25:58 +0200

    test: parameterized source tests

--- tests/crawl/test_main.py
+++ tests/crawl/test_main.py
@@ -1,6 +1,15 @@
+from typing import Generator, Callable
+
+import pytest
+
+from eventfully.database.schemas import Event
 from eventfully.crawl.source_list import SOURCES
 
 
-def test_crawl():
-    for source in SOURCES:
-        list(source())
+@pytest.mark.parametrize("source", SOURCES)
+def test_crawl(source: Callable[[], Generator[Event, None, None]]):
+    source_generator = source()
+    result = next(source_generator)
+    del source_generator
+
+    assert result
